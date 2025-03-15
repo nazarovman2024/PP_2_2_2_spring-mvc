9034b87d16f76ebbb2029c7989e3ddf8fb39fbab
nazarovman2024 <nazarovman2024@gmail.com>
2025-03-15 14:15:47 +0300

    20250315_JPP_2.2.2_fix0

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,26 +3,27 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
-import web.service.CarServiceImp;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CarController {
 
-    @Autowired
-    private CarService carService;
+    private final CarService carService;
 
     @GetMapping(value = "/cars")
     public String getCars(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
         List<Car> cars = carService.getCars(count);
         model.addAttribute("cars", cars);
         return "cars";
+    }
+
+    @Autowired
+    private CarController(CarService carService) {
+        this.carService = carService;
     }
 }
